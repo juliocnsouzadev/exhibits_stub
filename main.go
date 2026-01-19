66e1b1c9ef287455c014f0a0685f8a8d621205b7
Julio Souza <julio.cesar@scale.com>
2026-01-19 06:38:11 +0000

    Update main.go

--- main.go
+++ main.go
@@ -255,8 +255,8 @@ func main() {
 		}
 	})
 
-	log.Println("Starting server on :8089")
-	if err := http.ListenAndServe(":8089", nil); err != nil {
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
